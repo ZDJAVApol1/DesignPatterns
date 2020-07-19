d85d1be033f7ce710321ac46366858418c42d819
Marcin Szupke <marcin.szupke@gmail.com>
2020-07-19 11:33:15 +0200

    pizza app

--- src/main/java/pl/sda/zdjavapol1/factorymethod/pizza/PizzaApp.java
+++ src/main/java/pl/sda/zdjavapol1/factorymethod/pizza/PizzaApp.java
@@ -4,8 +4,19 @@ public class PizzaApp {
 
     public static void main(String[] args) {
 
+
+        // rodzaj ciasta - String
+        // sos - String
+        // dodatki - ArrayList<String>
+
+        // przygotowanie()
+        // pieczenie()
+        // krojenie()
+        // pakowanie()
+
 //        Pizzeria wloskaPizzeria = new WloskaPizzeria();
 //        Pizza wloskaPizzaSerowa = wloskaPizzeria.zamowPizza("serowa");
+//        System.out.println(wloskaPizzaSerowa);
 //
 //        Pizzeria amerykanskaPizzeria = new AmerykanskaPizzeria();
 //        Pizza amerykanskaPizzaSerowa = amerykanskaPizzeria.zamowPizza("serowa");
